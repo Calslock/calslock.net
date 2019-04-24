196b4a2a0172f788fd28e1e14be022ba0d3231d8
Karol Buchajczuk <omniquisi@gmail.com>
2019-04-24 23:10:43 +0200

    Version as of 24/04

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+﻿var i = 0;
+var txt = './calslock';
+
+function funny(){
+var random = Math.floor(Math.random()*3+1);
+switch(random){
+case 1: document.getElementById("title").innerHTML = "(´･ω･｀)"; break;
+case 2: document.getElementById("title").innerHTML = "(´^ω^｀)"; break;
+case 3: document.getElementById("title").innerHTML = "(づ￣ ³￣)づ"; break;
+}
+}
+
+function typeWriter(){
+  if (i<txt.length){
+    document.getElementById("title").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, 200);
+  }
+}
