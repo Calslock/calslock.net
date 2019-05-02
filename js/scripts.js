739be60e828995aa6b7e0b9644145d576047993a
Karol Buchajczuk <omniquisi@gmail.com>
2019-05-02 15:46:25 +0200

    Added clock

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
-﻿var i = 0;
-var txt = './calslock';
+﻿var i=0;
+var text="./calslock";
 
 function funny(){
 var random = Math.floor(Math.random()*3+1);
 switch(random){
-case 1: document.getElementById("title").innerHTML = "(´･ω･｀)"; break;
-case 2: document.getElementById("title").innerHTML = "(´^ω^｀)"; break;
-case 3: document.getElementById("title").innerHTML = "(づ￣ ³￣)づ"; break;
+  case 1: document.getElementById("title").innerHTML = "(´･ω･｀)"; break;
+  case 2: document.getElementById("title").innerHTML = "(´^ω^｀)"; break;
+  case 3: document.getElementById("title").innerHTML = "(づ￣ ³￣)づ"; break;
 }
 }
 
-function typeWriter(){
-  if (i<txt.length){
-    document.getElementById("title").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, 200);
+function titleAndDate(){
+  
+if (i<text.length){
+    document.getElementById("title").innerHTML += text.charAt(i);
+    i++; 
   }
+
+  var dt = new Date();
+  document.getElementById("clock").innerHTML = dt.toLocaleTimeString();
+  setTimeout(titleAndDate, 200);
 }
